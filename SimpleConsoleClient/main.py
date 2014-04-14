e92c4371c7609a0b08ef880ef706f22b4dc822c4
Timur <doxxerACM@gmail.com>
2014-04-15 00:45:29 +0400

    Client: requests over cookies

--- SimpleConsoleClient/main.py
+++ SimpleConsoleClient/main.py
@@ -1,30 +1,36 @@
+import cookielib
+
 from OneRequestWebServer import grab_authorization_code
 import requests
 
-# APPSERVER = "http://localhost:9080"
-APPSERVER = "https://spbau-notifier-583.appspot.com"
+
+APPSERVER = "http://localhost:9080"
+# APPSERVER = "https://spbau-notifier-583.appspot.com"
 
 LOGIN_ADDRESS = APPSERVER + "/login"
 LOGOUT_ADDRESS = APPSERVER + "/logout"
 AUTH_CALLBACK_ADDRESS = APPSERVER + "/oauth2callback"
 
+COOKIES = 'cookies.txt'
+
+session = requests.Session()
 
 def make_auth_request():
-    request = requests.post(LOGIN_ADDRESS).text
+    #s = requests.Session()
 
-    print request
+    response = session.post(LOGIN_ADDRESS).text
 
-    if request.split()[0].strip().lower() == "ok":
-        print request
+    if response.split()[0].strip().lower() == "ok":
+        print response
     else:
-        auth_code = grab_authorization_code(request)
-        request = requests.get(AUTH_CALLBACK_ADDRESS, params={'code': auth_code})
-        print request.status_code, request.reason, request.text
+        auth_code = grab_authorization_code(url=response)
+        response = session.get(AUTH_CALLBACK_ADDRESS, params={'code': auth_code})
+        print response.status_code, response.reason, response.text
 
 
 def make_logout_request():
-    request = requests.get(LOGOUT_ADDRESS)
-    print request.status_code, request.reason, request.text
+    response = session.get(LOGOUT_ADDRESS)
+    print response.status_code, response.reason, response.text
 
 
 def process_message(message):
@@ -51,5 +57,24 @@ def main():
         print
 
 
+def load_cookies_from_lwp(filename):
+    lwp_cookiejar = cookielib.LWPCookieJar()
+    lwp_cookiejar.load(filename, ignore_discard=True)
+    return lwp_cookiejar
+
+
+def save_cookies_lwp(cookiejar, filename):
+    lwp_cookiejar = cookielib.LWPCookieJar()
+    for c in cookiejar:
+        args = dict(vars(c).items())
+        args['rest'] = args['_rest']
+        del args['_rest']
+        c = cookielib.Cookie(**args)
+        lwp_cookiejar.set_cookie(c)
+    lwp_cookiejar.save(filename, ignore_discard=True)
+
+
 if __name__ == "__main__":
+    session.cookies = load_cookies_from_lwp(COOKIES)
     main()
+    save_cookies_lwp(session.cookies, COOKIES)
