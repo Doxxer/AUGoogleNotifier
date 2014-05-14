6b596a95de6351f30ffa535fc538b4b781f1556b
Timur <doxxerACM@gmail.com>
2014-05-14 23:47:29 +0400

    Fixed bug, when all changes're stored in 1 entity

--- AppServer/handlers/GetChangesHandler.py
+++ AppServer/handlers/GetChangesHandler.py
@@ -1,4 +1,5 @@
 import json
+import logging
 
 from ValidateCredential import ValidateCredential
 from handlers.BaseHandler import BaseHandler
@@ -10,10 +11,13 @@ class GetChangesHandler(BaseHandler):
     @ValidateCredential
     def post(self):
         user_id = self.session['user_id']
-        entity = GoogleDocsChange.get_or_insert('user_id', user_id=user_id)
+        entity = GoogleDocsChange.get_or_insert(str(user_id), user_id=user_id)
         changes = list(entity.change_ids)
         entity.change_ids[:] = []
         entity.save()
+
+        logging.warning(user_id)
+        logging.warning(changes)
 
         response = []
         for change_id in changes:
