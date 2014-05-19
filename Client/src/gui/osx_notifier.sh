4d4af6eed93d13b644173236ddbfd9c25fbea79b
Timur <doxxerACM@gmail.com>
2014-05-20 00:45:41 +0400

    Mac OS X notifier script updated

--- Client/src/gui/osx_notifier.sh
+++ Client/src/gui/osx_notifier.sh
@@ -4,36 +4,34 @@ usage() {
     echo "t -- title"
     echo "n -- user name"
     echo "u -- file URL"    
-    echo "p -- picture URL"
 }
 
 if [ "$#" -lt 1 ]; then
     usage
     exit 1
 fi
+
+URL="https://drive.google.com/?authuser=0#recent"
+FILENAME="Неизвестный документ"
+USERNAME="Неизвестный пользователь"
 s=""
+
 while getopts :t:u:n:p: option
 do
     case "${option}"
     in
-        t) TITLE=${OPTARG}
-            s="$s -title \"${OPTARG}\""
-            echo 1;;
-        n) s="$s -subtitle \"${OPTARG}\"";;
-        u) s="$s -open \"${OPTARG}\"";;
-        p) FORMAT=$OPTARG;;
+        t) FILENAME=$OPTARG;;
+        n) USERNAME=$OPTARG;;
+        u) URL=$OPTARG;;
     esac
     if [ -n "$a" ]; then
         echo $a
     fi    
 done
 
-if [[ -z $s ]]; then
-    usage
-    exit 1
-fi
-
+s="$s -title \"${FILENAME}\""
+s="$s -subtitle \"${USERNAME}\""
+s="$s -open \"${URL}\""
 s="$s -message \"Документ изменился\""
-s="$s -sender ru.spbau.mit.AUGoogleNotifier"
 s="terminal-notifier $s"
 eval $s
